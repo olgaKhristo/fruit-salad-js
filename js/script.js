0497861b80a9ae8bfe0edcf05289591f7c1fd13f
olgaKhristo <olga84h@gmail.com>
2023-09-11 15:02:18 +0100

    fetching data

--- js/script.js
+++ js/script.js
@@ -5,20 +5,32 @@
 const fruitForm = document.querySelector('#inputSection form')
 
 const fruitList = document.querySelector('#fruitSection ul')
+const fruitNutrition = document.querySelector('#nutritionSection p')
 
+const fetchFruitData = fruit => {
+    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+    .then(resp => resp.json())
+    .then(data => addFruit(data)
+    .catch(error =>{console.log(error)})    
+    ) } 
+
+
+    let totalCalories = 0
 const addFruit = fruit => {
     const li = document.createElement('li')
-    li.textContent = fruit;
+    li.textContent = fruit.name;
     fruitList.appendChild(li)
     li.addEventListener('click', () => {
         fruitList.removeChild(li)
+        totalCalories  =+ fruit.calories
+        fruitNutrition.textContent = `Total Calories: ${totalCalories}`
     })
 }
 
 
 fruitForm.addEventListener('submit', e =>  {
     e.preventDefault()
-    addFruit(e.target.fruitInput.value)
+    fetchFruitData(e.target.fruitInput.value)
     e.target.fruitInput.value = ''
 
 })
