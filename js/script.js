807a3a0e6b95863a93f1f4c738d0378b9e30d1d2
olgaKhristo <olga84h@gmail.com>
2023-09-11 15:39:09 +0100

    fixed code

--- js/script.js
+++ js/script.js
@@ -7,12 +7,16 @@ const fruitForm = document.querySelector('#inputSection form')
 const fruitList = document.querySelector('#fruitSection ul')
 const fruitNutrition = document.querySelector('#nutritionSection p')
 
-const fetchFruitData = fruit => {
-    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
-    .then(resp => resp.json())
-    .then(data => addFruit(data)
-    .catch(error =>{console.log(error)})    
-    ) } 
+const fetchFruitData =  async fruit => {
+    try{
+        const response = await fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+        const data = await response.json()
+        addFruit(data)
+    }catch(err){
+        console.log(err)
+    }  
+
+     } 
 
 
     let totalCalories = 0
@@ -20,11 +24,15 @@ const addFruit = fruit => {
     const li = document.createElement('li')
     li.textContent = fruit.name;
     fruitList.appendChild(li)
+    totalCalories += fruit.nutritions.calories;
+    fruitNutrition.textContent = `Total Calories: ${totalCalories.toFixed(2)}`
+  
     li.addEventListener('click', () => {
         fruitList.removeChild(li)
-        totalCalories  =+ fruit.calories
-        fruitNutrition.textContent = `Total Calories: ${totalCalories}`
-    })
+        fruitNutrition.removeChild(fruitNutrition.lastChild);
+    }
+        
+    )
 }
 
 
